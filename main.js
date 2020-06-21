81dd6e5c76eafe66037a9addd6086438a709191c
cundy01067 <57181197+cundy01067@users.noreply.github.com>
2020-06-21 16:10:12 +0800

    完成動畫設定

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-$('#banner').slick();
+AOS.init();
+
+$('#banner').slick({
+    autoplay:true,
+    autoplaySpeed:1500,
+    arrows:false
+});
+$('#brand').slick({
+    autoplay:true,
+    autoplaySpeed:1500,
+    dots:true,
+    slidesToShow:4,
+    arrows:false
+});
